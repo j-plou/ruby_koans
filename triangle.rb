34288ba65a867795d03174f0a167041449687ffd
j-plou <jesusplou@gmail.com>
2023-12-29 13:35:43 +0100

    Add exceptions control to triangle project

--- triangle.rb
+++ triangle.rb
@@ -14,7 +14,14 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  # WRITE THIS CODE
+  raise TriangleError if a <= 0 || b <= 0 || c <= 0
+  raise TriangleError if a + b <= c || a + c <= b || b + c <= a
+
+  case
+    when a == b && b == c && a == c then :equilateral
+    when a != b && b != c && a != c then :scalene
+    else                                 :isosceles
+  end
 end
 
 # Error class used in part 2.  No need to change this code.
