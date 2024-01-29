4f659f3e2e4f52330d03f24d1a7ce7190f2c6f64
j-plou <jesusplou@gmail.com>
2024-01-29 16:36:39 +0100

    Pass about proxy object project

--- about_proxy_object_project.rb
+++ about_proxy_object_project.rb
@@ -13,12 +13,29 @@ require File.expand_path(File.dirname(__FILE__) + '/neo')
 # of the Proxy class is given in the AboutProxyObjectProject koan.
 
 class Proxy
+  attr_reader(:messages)
+
   def initialize(target_object)
     @object = target_object
-    # ADD MORE CODE HERE
+    @messages = []
   end
 
-  # WRITE CODE HERE
+  def called?(method)
+    @messages.include?(method)
+  end
+
+  def number_of_times_called(method)
+    @messages.count(method)
+  end
+
+  def method_missing(method_name, *args)
+    if @object.respond_to?(method_name)
+      @messages.push(method_name)
+      @object.__send__(method_name, *args)
+    else
+      raise NoMethodError.new("[#{method_name}] method doesn't exists")
+    end
+  end
 end
 
 # The proxy object should pass the following Koan:
